0c9f9943ff58de510a77e1593b9ac4beec17ea37
KaterynaDvernytska <dvernitskayae@gmail.com>
2023-06-05 02:35:42 +0300

    another message

--- Src/Lesson4b.java
+++ Src/Lesson4b.java
@@ -1,5 +1,7 @@
 public class Lesson4b {
    public static void main(String[] args){
        System.out.println("Hello git");
+       System.out.println("Hello git from Kateryna Dvernytska");
+
    }
 }
